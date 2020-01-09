b16cdde7b7a1d3530396f6628f118071e6e875c7
Your Name <you@example.com>
2020-01-08 17:49:53 -0800

    two new files

---
+++ arithmetic_2.py
@@ -0,0 +1,59 @@
+"""Math functions for calculator."""
+
+
+def add(tokens):
+    """Return the sum of the two inputs."""
+    x = 0
+    for i in range(len(tokens)):
+        if i > 0:
+            x += tokens[i]
+       # if type(i) == type(float)
+        #    x += i
+    return x
+
+
+def subtract(tokens):
+    """Return the second number subtracted from the first."""
+    x = tokens[1]
+    for i in range(len(tokens)):
+        if i > 1:
+            x -= tokens[i]
+    return x
+
+def multiply(num1, num2):
+    """Multiply the two inputs together."""
+    x = num1 * num2
+    return x
+
+def divide(num1, num2):
+    """Divide the first input by the second and return the result."""
+    x = num1 / num2
+    return x
+
+def square(num1):
+    """Return the square of the input."""
+    x = num1 * num1
+    return x
+
+def cube(num1):
+    """Return the cube of the input."""
+    x = num1 ** 3
+    return x
+
+def power(num1, num2):
+    """Raise num1 to the power of num2 and return the value."""
+    x = num1 ** num2
+    return x        
+
+def mod(num1, num2):
+    """Return the remainder of num1 / num2."""
+    x = num1 % num2
+    return x
+
+def add_mult(num1, num2, num3):
+    x = (num1 + num2) * num3
+    return x
+
+def add_cubes(num1, num2):
+    x = num1**3 + num2**3
+    return x
---
+++ calculator-2.py
@@ -0,0 +1,49 @@
+from arithmetic_2 import *
+
+def user_input():
+
+    prefix = input("> ")
+    tokens = prefix.split(" ")
+    for i in range(len(tokens)):
+        if i > 0:
+            tokens[i] = float(tokens[i])
+
+    print(tokens)
+    return tokens
+
+
+def call_arithmetic_functions(tokens):
+    if tokens[0] == "+":
+        print(add(tokens))
+    elif tokens[0] == "-":
+        print(subtract(tokens))
+    elif tokens[0] == "*":
+        print(multiply(tokens[1], tokens[2]))
+    elif tokens[0] == "/":
+        print(divide(tokens[1], tokens[2]))
+    elif tokens[0] == "square":
+        print(square(tokens[1]))
+    elif tokens[0] == "cube":
+        print(cube(tokens[1]))
+    elif tokens[0] == "pow":
+        print(power(tokens[1], tokens[2]))
+    elif tokens[0] == "mod":    
+        print(mod(tokens[1], tokens[2]))
+    elif tokens[0] == "x+":
+        print(add_mult(tokens[1], tokens[2], tokens[3]))
+    elif tokens[0] == "cube+":
+        print(add_cubes(tokens[1], tokens[2]))
+    else:
+        print("INVALID INPUT, PLEASE TRY AGAIN!")
+
+
+while True:
+
+    tokens = user_input()
+
+    if tokens[0] == "Q" or tokens[0] == "q" or tokens[0] == "Quit" or tokens[0] == "quit" or tokens[0] == "QUIT":
+        print("Good riddance!")
+        break
+
+    call_arithmetic_functions(tokens)
+
